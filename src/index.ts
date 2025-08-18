4eaa112f288821735c712c76f40af8f1bb41ba84
vansh123456 <vanshbhatnagar14dev@gmail.com>
2025-08-18 18:27:53 +0530

    backend: add essential server init

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import * as dynamoose from "dynamoose";
+import serverless from "serverless-http";
+import seed from "./seed/seedDynamodb";
+/* ROUTE IMPORTS */
+import courseRoutes from "./routes/courseRoutes";
+import userClerkRoutes from "./routes/userClerkRoutes";
+import transactionRoutes from "./routes/transactionRoutes";
+import userCourseProgressRoutes from "./routes/userCourseProgressRoutes";
+
+/* CONFIGURATIONS */
+dotenv.config();
+const isProduction = process.env.NODE_ENV === "production";
+if (!isProduction) {
+  dynamoose.aws.ddb.local();
+}
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.use("/courses",courseRoutes);
+
+/* SERVER */
+const port = process.env.PORT || 3000;
+if (!isProduction) {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
